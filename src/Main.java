4af1adbf0aed333ce3ddd6965664b1112e1ae623
wthered <wthered@gmail.com>
2016-11-12 16:18:41 +0200

    Source Update with Player Attributes (Strength, Agility etc)

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        System.out.println("Main.main");
+        String username;
+        String password;
+        int action = 0;
 
-        int action = createOrLogin();
+        System.out.println("Main.main");
+        System.out.print("What is you username? ");
+        Scanner userScan = new Scanner(System.in);
+        username = userScan.nextLine();
+
+        System.out.print("What is your Password? ");
+        password = userScan.nextLine();
+
+        boolean identify = auth(username, password);
+
+        if (identify) {
+            action = createOrLogin();
+        }
+
         System.out.println("Last action was " + action);
     }
 
+    private static boolean auth(String username, String password) {
+        System.out.println("Server Authentication in Main.auth");
+        System.out.println("Username\t" + username);
+        System.out.println("Password\t" + password);
+        return true;
+    }
+
     private static int createOrLogin() {
-        return 3;
+        System.out.println("**************************");
+        System.out.println("** 1) Create Character  **");
+        System.out.println("** 2) Resume saved Game **");
+        System.out.println("**************************");
+        System.out.print("What is your desired action? ");
+        Scanner actScan = new Scanner(System.in);
+        return actScan.nextInt();
     }
 }
